9eb7dee164189385b4bac0c73b6e571476c18afe
Emanuel Gifuni <s194407@student.pg.edu.pl>
2024-02-18 22:58:55 +0100

    Random walk

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -1,15 +1,12 @@
 import matplotlib.pyplot as plt
 from randomwalk import RandomWalk
 
-rw = RandomWalk(150_000)
+rw = RandomWalk(5_000)
 rw.fill_walk()
 
 fig, ax = plt.subplots()
 point_numbers = range(rw.num_points)
-ax.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = plt.cm.Blues, edgecolor = 'none', s = 1)
-ax.scatter( 0, 0, c = 'green', edgecolor = 'none', s = 5)
-ax.scatter( rw.x_values[-1], rw.y_values[-1], c = 'red', edgecolor = 'none', s = 5)
-
+ax.plot(rw.x_values, rw.y_values, linewidth = 2)
 ax.get_xaxis().set_visible(False)
 ax.get_yaxis().set_visible(False)
 
--- randomwalk.py
+++ randomwalk.py
@@ -9,15 +9,11 @@ class RandomWalk():
 
     def fill_walk(self):
         while len(self.x_values) < self.num_points:
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
 
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
-            if x_step == 0 and y_step ==0: 
+            if x_step == 0 and y_step == 0: 
                 continue
 
             x = self.x_values[-1] + x_step
@@ -26,4 +22,10 @@ class RandomWalk():
             self.x_values.append(x)
             self.y_values.append(y)
 
+    def get_step(self):
+        direction = choice([1, -1])
+        distance = choice([1, -1])
+        step = direction * distance
+
+        return step
 
